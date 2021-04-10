12691019775c2ff300661e65bde09de305f51f6c
Paweł Tomaszek <ptomaszek@users.noreply.github.com>
2021-04-10 13:31:35 +0200

    Fix coords format

--- src/content.js
+++ src/content.js
@@ -2,8 +2,9 @@ chrome.runtime.onMessage.addListener(function (msg, sender, callback) {
     if (msg.action === 'getGpsCoords') {
         if (document.querySelectorAll(".infoStripe .iy em")[1] != null) {
             callback({
-                lat: document.querySelectorAll(".infoStripe .ix em")[1].innerText,
-                lon: document.querySelectorAll(".infoStripe .iy em")[1].innerText
+                //fix the separator sign if Geoportal lang in Polish
+                lat: document.querySelectorAll(".infoStripe .ix em")[1].innerText.replace(',', '.'),
+                lon: document.querySelectorAll(".infoStripe .iy em")[1].innerText.replace(',', '.')
             });
         }
     }
